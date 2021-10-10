e697ae5a119c4846812c3e0f599d2603695aeb2a
Sebastiaan Janssen <sebastiaan@umbraco.com>
2021-10-10 11:36:00 +0200

    Check if there's a connection string before doing anything with it
    Move adding dashboard to separate method

--- Cultiv.Hangfire/HangfireComposer.cs
+++ Cultiv.Hangfire/HangfireComposer.cs
@@ -18,26 +18,38 @@ namespace Cultiv.Hangfire
         {
             // Configure Hangfire to use our current database and add the option to write console messages
             var connectionString = builder.Config.GetConnectionString(Umbraco.Cms.Core.Constants.System.UmbracoConnectionName);
-            builder.Services.AddHangfire(configuration =>
+            if (string.IsNullOrEmpty(connectionString) == false)
             {
-                configuration
-                    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
-                    .UseSimpleAssemblyNameTypeSerializer()
-                    .UseRecommendedSerializerSettings()
-                    .UseConsole()
-                    .UseSqlServerStorage(connectionString, new SqlServerStorageOptions
-                    {
-                        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
-                        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
-                        QueuePollInterval = TimeSpan.Zero,
-                        UseRecommendedIsolationLevel = true,
-                        DisableGlobalLocks = true,
-                    });
-            });
-            
-            // Run the required server so your queued jobs will get executed
-            builder.Services.AddHangfireServer();
-            
+                builder.Services.AddHangfire(configuration =>
+                {
+                    configuration
+                        .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                        .UseSimpleAssemblyNameTypeSerializer()
+                        .UseRecommendedSerializerSettings()
+                        .UseConsole()
+                        .UseSqlServerStorage(connectionString, new SqlServerStorageOptions
+                        {
+                            CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                            SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                            QueuePollInterval = TimeSpan.Zero,
+                            UseRecommendedIsolationLevel = true,
+                            DisableGlobalLocks = true,
+                        });
+                });
+
+                // Run the required server so your queued jobs will get executed
+                builder.Services.AddHangfireServer();
+
+                AddAuthorizedUmbracoDashboard(builder);
+                
+                // For some reason we need to give it the connection string again, else we get this error:
+                // https://discuss.hangfire.io/t/jobstorage-current-property-value-has-not-been-initialized/884
+                JobStorage.Current = new SqlServerStorage(connectionString);
+            }
+        }
+
+        private static void AddAuthorizedUmbracoDashboard(IUmbracoBuilder builder)
+        {
             // Add a named policy to authorize requests to the dashboard
             builder.Services.AddAuthorization(options =>
             {
@@ -48,7 +60,7 @@ namespace Cultiv.Hangfire
                     policy.Requirements.Add(new SectionRequirement(Umbraco.Cms.Core.Constants.Applications.Settings));
                 });
             });
-            
+
             // Add the dashboard and make sure it's authorized with the named policy above
             builder.Services.Configure<UmbracoPipelineOptions>(options =>
             {
@@ -57,15 +69,11 @@ namespace Cultiv.Hangfire
                     Endpoints = app => app.UseEndpoints(endpoints =>
                     {
                         endpoints.MapHangfireDashboard(
-                            pattern: "/umbraco/backoffice/hangfire",
-                            options: new DashboardOptions()).RequireAuthorization(Constants.System.HangfireDashboard);
+                                pattern: "/umbraco/backoffice/hangfire",
+                                options: new DashboardOptions()).RequireAuthorization(Constants.System.HangfireDashboard);
                     }).UseHangfireDashboard()
                 });
             });
-            
-            // For some reason we need to give it the connection string again, else we get this error:
-            // https://discuss.hangfire.io/t/jobstorage-current-property-value-has-not-been-initialized/884
-            JobStorage.Current = new SqlServerStorage(connectionString);
         }
     }
 }
