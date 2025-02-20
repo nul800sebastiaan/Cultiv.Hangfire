a3424d265369e0b110bcdd759e91136064561312
Sebastiaan Janssen <sebastiaan@umbraco.com>
2025-02-20 12:32:07 +0100

    Refactor constants and move to better classname, introduce some new constants

--- Cultiv.Hangfire/Constants.cs
+++ Cultiv.Hangfire/Constants.cs
@@ -2,12 +2,29 @@
 {
     public static class Constants
     {
+        [Obsolete("Use Constants.Cultiv instead")]
         public static class System
+        {
+            [Obsolete("Use Constants.CultivHangfire.HangfireDashboard instead")]
+            public const string HangfireDashboard = CultivHangfire.HangfireDashboard;
+            [Obsolete("Use Constants.CultivHangfire.Endpoint instead")]
+            public const string Endpoint = CultivHangfire.Endpoint;
+            [Obsolete("Use Constants.CultivHangfire.AlternativeConnectionStringName instead")]
+            public const string AlternativeConnectionStringName = CultivHangfire.AlternativeConnectionStringName;
+            
+            [Obsolete("Authentication policies are no longer used, moved to cookie authentication")]
+            public const string AuthenticationPolicyName = "Cultiv.Hangfire.AuthenticationPolicy";
+        }
+        
+        public static class CultivHangfire
         {
             public const string HangfireDashboard = nameof(HangfireDashboard);
             public const string Endpoint = "/umbraco/hangfire";
             public const string AlternativeConnectionStringName = "hangfireDB";
-            public const string AuthenticationPolicyName = "Cultiv.Hangfire.AuthenticationPolicy";
+            
+            public const string CookiesScheme = "Cultiv.Hangfire.CookieScheme";
+            public const string CookieName = "Cultiv.Hangfire";
+            public const string ClaimType = "HangfireAllowed";
         }
     }
 }
