036271e4d9aa25ef1990a7e465dda531ed7be6ce
Sebastiaan Janssen <sebastiaan@umbraco.com>
2025-02-20 12:46:58 +0100

    Switch to cookie authorization with the a custom cookie schema

---
+++ Cultiv.Hangfire/HangfireCookieAuthorizationFilter.cs
@@ -0,0 +1,13 @@
+﻿using Hangfire.Dashboard;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Cultiv.Hangfire;
+
+public class HangfireCookieAuthorizationFilter : IDashboardAsyncAuthorizationFilter
+{
+    public async Task<bool> AuthorizeAsync(DashboardContext context)
+    {
+        var result = await context.GetHttpContext().AuthenticateAsync(Constants.CultivHangfire.CookiesScheme);
+        return result.Succeeded;
+    }
+}
--- Cultiv.Hangfire/UmbracoBuilderExtensions.cs
+++ Cultiv.Hangfire/UmbracoBuilderExtensions.cs
@@ -1,9 +1,7 @@
 ﻿using Hangfire;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Web.Common.ApplicationBuilder;
-using Umbraco.Extensions;
 
 namespace Cultiv.Hangfire;
 
@@ -12,36 +10,33 @@ public static class UmbracoBuilderExtensions
     public static string? GetConnectionString(this IUmbracoBuilder builder)
     {
         var connectionString =
-            builder.Config.GetUmbracoConnectionString(Constants.System.AlternativeConnectionStringName);
+            builder.Config.GetUmbracoConnectionString(Constants.CultivHangfire.AlternativeConnectionStringName);
         return string.IsNullOrWhiteSpace(connectionString) ? builder.Config.GetUmbracoConnectionString() : connectionString;
     }
     
     internal static void AddHangfireToUmbraco(this IUmbracoBuilder builder, bool serverDisabled)
     {
-        builder.Services.AddAuthorizationBuilder()
-            .AddPolicy(Constants.System.AuthenticationPolicyName, policy =>
-            {
-                policy
-                    .AddAuthenticationSchemes(Umbraco.Cms.Core.Constants.Security.BackOfficeAuthenticationType)
-                    .RequireClaim(Umbraco.Cms.Core.Constants.Security.AllowedApplicationsClaimType, Umbraco.Cms.Core.Constants.Applications.Settings)
-                    .RequireAuthenticatedUser();
-            });
-
         if (!serverDisabled)
         {
             builder.Services.AddHangfireServer();
         }
+        else
+        {
+            return;
+        }
 
         builder.Services.Configure<UmbracoPipelineOptions>(options =>
         {
-            options.AddFilter(new UmbracoPipelineFilter(Constants.System.HangfireDashboard)
+            options.AddFilter(new UmbracoPipelineFilter(Constants.CultivHangfire.HangfireDashboard)
             {
                 Endpoints = app => app.UseEndpoints(endpoints =>
                 {
                     endpoints.MapHangfireDashboard(
-                            pattern: Constants.System.Endpoint,
-                            options: new DashboardOptions { IgnoreAntiforgeryToken = true })
-                        .RequireAuthorization(Constants.System.AuthenticationPolicyName);
+                        pattern: Constants.CultivHangfire.Endpoint,
+                        options: new DashboardOptions
+                        {
+                            AsyncAuthorization = [ new HangfireCookieAuthorizationFilter() ]
+                        });
                 })
             });
         });
