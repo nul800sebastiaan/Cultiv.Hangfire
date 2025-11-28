85fc034d71581147b28b28ff7046175b57cb25e5
Sebastiaan Janssen <sebastiaan@umbraco.com>
2025-11-28 15:37:21 +0100

    Remove obsolete settings class for new Major release version

--- Cultiv.Hangfire/Constants.cs
+++ Cultiv.Hangfire/Constants.cs
@@ -1,21 +1,7 @@
 ﻿namespace Cultiv.Hangfire
 {
     public static class Constants
-    {
-        [Obsolete("Use Constants.Cultiv instead")]
-        public static class System
-        {
-            [Obsolete("Use Constants.CultivHangfire.HangfireDashboard instead")]
-            public const string HangfireDashboard = CultivHangfire.HangfireDashboard;
-            [Obsolete("Use Constants.CultivHangfire.Endpoint instead")]
-            public const string Endpoint = CultivHangfire.Endpoint;
-            [Obsolete("Use Constants.CultivHangfire.AlternativeConnectionStringName instead")]
-            public const string AlternativeConnectionStringName = CultivHangfire.AlternativeConnectionStringName;
-            
-            [Obsolete("Authentication policies are no longer used, moved to cookie authentication")]
-            public const string AuthenticationPolicyName = "Cultiv.Hangfire.AuthenticationPolicy";
-        }
-        
+    {        
         public static class CultivHangfire
         {
             public const string HangfireDashboard = nameof(HangfireDashboard);
